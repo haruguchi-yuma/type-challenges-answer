1acdcb3ab1d28c42eafabadc441cfc516ed2928e
yuma <ikeoku.y@gmail.com>
2022-07-08 11:50:55 +0900

    Arrayの最後の要素を抽出

---
+++ src/medium/00015-medium-last.ts
@@ -0,0 +1,13 @@
+{
+
+  type arr1 = ['a', 'b', 'c']
+  type arr2 = [3, 2, 1]
+
+  type Last<T extends any[]> = T extends [...any, infer L] ? L : never
+
+  type tail1 = Last<arr1> // expected to be 'c'
+  type tail2 = Last<arr2> // expected to be 1
+
+}
+
+// Type Lstt<T extends any[]> = T extends [...infer R, infer L] ? L : neverと書いてる人が多かった。
