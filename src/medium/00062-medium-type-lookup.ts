af2c26e567d2edff003350d9c6562ed666b3655a
yuma <ikeoku.y@gmail.com>
2022-07-12 14:55:59 +0900

    Lookupの実装

---
+++ src/medium/00062-medium-type-lookup.ts
@@ -0,0 +1,14 @@
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type LookUp<U, K> = U extends { type: K } ? U : never;
+
+type MyDog = LookUp<Cat | Dog, 'dog'> // expected to be `Dog`
